c32812ef34d152c181ad2590ad13f8a2201fa347
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-05 08:37:49 +0800

    Added Prog03.py

---
+++ Prog03.py
@@ -0,0 +1,8 @@
+# Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+minusnum = num1 - num2
+
+print(minusnum)
