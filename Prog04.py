f10498e16eb215177fc677924561aa6302c42e11
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-05 08:42:05 +0800

    Added Prog04.py

---
+++ Prog04.py
@@ -0,0 +1,8 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+
+quotientnum = num1 // num2
+
+print(quotientnum)
