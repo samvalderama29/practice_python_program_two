48cf1dbda7f3cbec5f56056bf9b80a7375706f9a
Samantha Angel E. Valderama <samanthaangelvalderama@gmail.com>
2025-03-05 09:44:16 +0800

    Added Prog10.py

---
+++ Prog10.py
@@ -0,0 +1,11 @@
+# Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+if num1 > num2:
+    for between_num in range(num1, num2 + 1):
+        print(between_num)
+else:
+    for between_num in range(num1, num2 + 1):
+        print(between_num)
